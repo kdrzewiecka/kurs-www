d44ba04a70ae6388cdf37422a01ffdc1a7d16811
kdrzewiecka <kejtka@wp.pl>
2022-03-31 15:38:40 +0200

    31.01

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello Java Script")
+// STAŁA (nie mozna jej zmieniać)
+const z = 5;
+// ZMIENNA (może sie zmieniać)
+let x = 7;
+// STRING ( ciag znaków -zawsze cudzysłowiu)
+const string = "asd"
+
+// OBIEKTY 
+const project ={
+    name : "Projekt 1",
+    date : "24.11.20018"
+}
+
+
+
+console.log("Project o nazwie"+project.name)
++ "został stworzony" project.date
