232b7d6f15854ecf2bac27a45ff8577cd3278b0c
kdrzewiecka <kejtka@wp.pl>
2022-03-31 15:44:08 +0200

    31.03

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ const string = "asd"
 // OBIEKTY 
 const project = {
     name : "Projekt 1",
-    date : "24.11.2018"
+    date : "24.11.2018",
+    id = 1,
+    child : {
+        name: "ChlildProject"
+    }
 }
-
-
-
 //console.log("Project o nazwie"+project.name)
 //+ "został stworzony" project.date
+
+console.log(console)
+
+//TABLICE
+//const fibArray =
