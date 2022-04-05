f07ee5537cb78068be7466535f7bf83cb324b881
kdrzewiecka <kejtka@wp.pl>
2022-04-05 16:22:30 +0200

    java 2 zaj

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log(document.querySelector("#projects .cItem"))
+
+//Projekty
+
+//stała allCIItem, przechowująca tablicę ("el1, el2") wszystkich klas .cItems w #projects
+
+const allCIItem = document.querySelectorAll( "#projects.cItem");
+// wyciagamy każdy .Items z osobna
+
+allCIItem.forEach(  cItem => {
+    // kazdemu po kliknięciu dajemy funkcje strzałkowa
+
+    cItems.onclik = () => {
+    // cItem.addEventListner ("mouseover",() => {
+        //usuń klasę .active ze wszystkich elelmentów 
+    allCIItem.forEach( cItem2 => {
+        cItem2.classList.remove("active")
+})
+
+// do kliknietego elementu dopisz klasę "".active"
+cItem.classList.add ("active")
+    }
+
+// )// (jeśli używasz event listner)
+})
